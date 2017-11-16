fdd7ad38a1153460fa5aadc489374f80a1f33386
hqkhai <khai.ho@axonactive.com>
2017-11-16 16:54:17 +0700

    master: sort list of candidate by update date

--- NovaDevday/src/nova/devday/persistent/CrawlingPersistentService.java
+++ NovaDevday/src/nova/devday/persistent/CrawlingPersistentService.java
@@ -8,12 +8,13 @@ import ch.ivyteam.ivy.environment.Ivy;
 
 public class CrawlingPersistentService {
 		
-
 		public String addCandidate(CandidateInfo candidateInfo){
 			return Ivy.repo().save(candidateInfo).getId();
 		}
 		
 		public List<CandidateInfo> getCandidateInfoList(){
-			return Ivy.repo().search(CandidateInfo.class).limit(10000).execute().getAll();
+			List<CandidateInfo> list = Ivy.repo().search(CandidateInfo.class).limit(10000).execute().getAll();
+			list.sort((CandidateInfo a, CandidateInfo b) -> (int) (b.getUpdatedDate().getTime() - a.getUpdatedDate().getTime()) / 1000);
+			return list;
 		}
 }
