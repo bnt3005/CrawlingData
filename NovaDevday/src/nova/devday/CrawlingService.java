3f8c86c6d768239cc9fc304af9bd3275a2653bab
nvcuong387 <cuong.nguyenviet@axonactive.com>
2017-11-16 11:24:17 +0700

    Cuong69 update CrawlingService and datamodel

--- NovaDevday/src/nova/devday/CrawlingService.java
+++ NovaDevday/src/nova/devday/CrawlingService.java
@@ -1,15 +1,13 @@
 package nova.devday;
 
 import java.io.IOException;
-import java.util.Iterator;
-
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.Connection.Method;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-
+import com.nova.devday.CandidateInfo;
 import ch.ivyteam.ivy.environment.Ivy;
 
 public class CrawlingService {
@@ -22,10 +20,6 @@ public class CrawlingService {
 	private static final String LOGIN_LINK = SERVER_HOST + "/v2/login";
 	private static final String QUERY_STRING = SERVER_HOST + "/v2/resume/search?searchResume%5Bkeyword%5D=java&searchResume%5BjobLevelId%5D=&searchResume%5BlastModified%5D=3&btnResumeSearch=1&searchResume%5ByearExperience%5D=&searchResume%5Bnationality%5D=&searchResume%5Blanguage%5D=&searchResume%5BlanguageLevel%5D=&searchResume%5BageFrom%5D=&searchResume%5BageTo%5D=&searchResume%5BsalaryFrom%5D=&searchResume%5BsalaryTo%5D=&searchResume%5BgenderId%5D=";
 
-	public static void main(String[] args) throws IOException {
-		crawlData();
-	}
-
 	public static void crawlData() throws IOException {
 		Connection.Response initial = Jsoup.connect(LOGIN_LINK).method(Connection.Method.GET).execute();
 
@@ -36,54 +30,83 @@ public class CrawlingService {
 		crawlDataFromUrl(login, QUERY_STRING, 1);
 		// For other pages
 		Document pageContent = Jsoup.connect(QUERY_STRING).cookies(login.cookies()).get();
-		Elements pagi = pageContent.select("div[class=pagination btn-group");
-		Elements paginations = pagi.select("button[data-url]");
-		Integer i = 1;
+		Elements paginationPanel = pageContent.select("div[class=pagination btn-group");
+		Elements paginations = paginationPanel.select("button[data-url]");
+		Integer pageNo = 1;
 		for (Element pagination : paginations) {
-			i++;
-			crawlDataFromUrl(login, SERVER_HOST + pagination.attr("data-url"), i);
+			pageNo++;
+			crawlDataFromUrl(login, SERVER_HOST + pagination.attr("data-url"), pageNo);
 		}
 	}
 
-	private static void crawlDataFromUrl(Connection.Response login, String url, Integer pageNum) throws IOException {
+	private static void crawlDataFromUrl(Connection.Response login, String url, Integer pageNo) throws IOException {
 		Document pageContent = Jsoup.connect(url).cookies(login.cookies()).get();
 		Elements employeeLinks = pageContent.select("a[href]");
-		int i = 0;
+		int candidateNo = 0;
 		for (Element employeeLink : employeeLinks) {
 			if (employeeLink.attr("href").contains("resume/detail")) {
-				i++;
-				Ivy.log().info("Page: " + pageNum + ". No: " + i + "-------------------------" + SERVER_HOST
-						+ employeeLink.attr("href") + "--------------------------");
-				Document employeePage = Jsoup.connect(SERVER_HOST + employeeLink.attr("href")).cookies(login.cookies())
-						.get();
+				employeeLink.attr("href").substring(employeeLink.attr("href").length() - employeeLink.attr("href").indexOf("/"));
+				String candidateId = employeeLink.attr("href").replace("/v2/resume/detail/", "");
+				candidateNo++;
+				CandidateInfo candidateInfo = new CandidateInfo();
+				candidateInfo.setCandidateId(candidateId);
+				candidateInfo.setProfileLink(SERVER_HOST + employeeLink.attr("href"));
+				candidateInfo.setPageNo(pageNo);
+				candidateInfo.setCandidateNo(candidateNo);
+				Document employeePage = Jsoup.connect(SERVER_HOST + employeeLink.attr("href")).cookies(login.cookies()).get();
 				Elements leadContactResumtBtn = employeePage.select("button[id=leadContactResumtBtn");
 				String leadContactResumtLink = leadContactResumtBtn.attr("abs:data-url");
-				Ivy.log().info("Click link to buy: " + leadContactResumtLink);
+				candidateInfo.setContactLink(leadContactResumtLink);
 				Elements infoTable = employeePage.select("table[class=table m-b-xs m-t");
-				Iterator<Element> infoEachRow = infoTable.select("td").iterator();
-				Ivy.log().info(printInformation(infoEachRow));
-				Ivy.log().info(printInformation(infoEachRow));
-				Ivy.log().info(printInformation(infoEachRow));
-				Ivy.log().info(printInformation(infoEachRow));
-				Ivy.log().info(printInformation(infoEachRow));
-				Ivy.log().info(printInformation(infoEachRow));
-				Ivy.log().info(printInformation(infoEachRow));
-				Ivy.log().info(printInformation(infoEachRow));
-				Ivy.log().info(printInformation(infoEachRow));
-				Ivy.log().info(printInformation(infoEachRow));
-				Ivy.log().info(printInformation(infoEachRow));
+				Elements infoRows = infoTable.select("td");
+				int infoNo = 1;
+				for (Element infoRow : infoRows) {
+					if(infoNo % 2 == 0 && infoRow.hasText()){
+						initCandidateInfo(infoNo, candidateInfo, infoRow.text());
+					}
+					infoNo++;
+				}
+				Ivy.log().info(candidateInfo);
 			}
 		}
 	}
 	
-	private static String printInformation(Iterator<Element> infoEachRow){
-		String result = "";
-		if (infoEachRow.hasNext()){
-			result = infoEachRow.next().text();
+	private static void initCandidateInfo(Integer i, CandidateInfo candidateInfo, String info){
+		switch(i){
+		case 2: 
+			candidateInfo.setHighestEducation(info);
+			break;
+		case 4: 
+			candidateInfo.setYearsOfExperience(info);
+			break;
+		case 6: 
+			candidateInfo.setLanguages(info);
+			break;
+		case 8: 
+			candidateInfo.setMostRecentJob(info);
+			break;
+		case 10: 
+			candidateInfo.setLatestCompany(info);
+			break;
+		case 12: 
+			candidateInfo.setExperienceLevel(info);
+			break;
+		case 14: 
+			candidateInfo.setExpectedPosition(info);
+			break;
+		case 16: 
+			candidateInfo.setExpectedJobLevel(info);
+			break;
+		case 18: 
+			candidateInfo.setJobLocations(info);
+			break;
+		case 20: 
+			candidateInfo.setJobIndustriesFunctions(info);
+			break;
+		case 22: 
+			candidateInfo.setMinimumExpectedSalary(info);
+			break;
 		}
-		if (infoEachRow.hasNext()){
-			result += ": " + infoEachRow.next().text();
-		}
-		return result;
 	}
+	
 }
