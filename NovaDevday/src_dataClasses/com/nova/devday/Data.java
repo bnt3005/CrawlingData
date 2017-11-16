be7f7d7f1cac2897ee80758c57212fe85938792e
Cuong Tran <cuong.tran@axonactive.com>
2017-11-16 17:55:00 +0700

    Remove unused variable

--- NovaDevday/src_dataClasses/com/nova/devday/Data.java
+++ NovaDevday/src_dataClasses/com/nova/devday/Data.java
@@ -7,26 +7,6 @@ package com.nova.devday;
 public class Data extends ch.ivyteam.ivy.scripting.objects.CompositeObject
 {
   /** SerialVersionUID */
-  private static final long serialVersionUID = -7427566118438272494L;
-
-  private java.lang.Boolean isCrawlingTime;
-
-  /**
-   * Gets the field isCrawlingTime.
-   * @return the value of the field isCrawlingTime; may be null.
-   */
-  public java.lang.Boolean getIsCrawlingTime()
-  {
-    return isCrawlingTime;
-  }
-
-  /**
-   * Sets the field isCrawlingTime.
-   * @param _isCrawlingTime the new value of the field isCrawlingTime.
-   */
-  public void setIsCrawlingTime(java.lang.Boolean _isCrawlingTime)
-  {
-    isCrawlingTime = _isCrawlingTime;
-  }
+  private static final long serialVersionUID = 0L;
 
 }
