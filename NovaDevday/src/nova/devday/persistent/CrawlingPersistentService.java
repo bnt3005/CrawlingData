1a9c20d050f0946f27aca94e1ac99b781f348d75
binhnguyen <binh.nguyen@axonactive.com>
2017-11-16 10:28:12 +0700

    Add data model

--- NovaDevday/src/nova/devday/persistent/CrawlingPersistentService.java
+++ NovaDevday/src/nova/devday/persistent/CrawlingPersistentService.java
@@ -7,13 +7,14 @@ import com.nova.devday.CandidateInfo;
 import ch.ivyteam.ivy.environment.Ivy;
 
 public class CrawlingPersistentService {
-
 		
+		private static final String PERSISTENT_NAME = "nova_candidate_crawling";
+
 		public CandidateInfo addCandidate(CandidateInfo candidateInfo){
-			return Ivy.persistence().get("nova_candidate_crawling").persist(candidateInfo);
+			return Ivy.persistence().get(PERSISTENT_NAME).persist(candidateInfo);
 		}
 		
 		public List<CandidateInfo> getCandidateInfoList(){
-			return Ivy.persistence().get("nova_candidate_crawling").findAll(CandidateInfo.class);
+			return Ivy.persistence().get(PERSISTENT_NAME).findAll(CandidateInfo.class);
 		}
 }
